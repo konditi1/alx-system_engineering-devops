fe98827c15a088cb2f3315d87a719e8cba440f12
konditi1 <konditi1@gmail.com>
2023-09-22 23:52:20 +0300

    using pkill to kill a procces

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # manifest that kills a process named killmenow.
 exec { 'pkill':
-    command      => 'pkill -f killmenow':
-        provider => 'shell',
-    }
+    command  => 'pkill -f killmenow':
+    provider => 'shell',
+  }
