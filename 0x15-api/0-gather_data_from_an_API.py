df96f86cac5171cc1487dce5a807056ef87f4ebd
konditi1 <konditi1@gmail.com>
2023-10-23 18:50:53 +0300

    corrected pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -35,8 +35,8 @@ def get_employee_todo_progress(employee_id):
     done_tasks = sum(1 for task in todo_list if task["completed"])
 
     # Display progress
-    print(f"Employee {employee_name}"
-          f"is done with tasks({done_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks\
+    ({done_tasks}/{total_tasks}):")
     for task in todo_list:
         if task["completed"]:
             print(f"\t{task['title']}")
