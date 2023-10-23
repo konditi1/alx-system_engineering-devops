214d795169d983fff26c6e0c80eb2f11479869ff
konditi1 <konditi1@gmail.com>
2023-10-23 18:32:53 +0300

    Retrieve and display the TODO list progress for a given employee.

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+"""
+    Retrieve and display the TODO list progress for a given employee.
+"""
+
+
+def get_employee_todo_progress(employee_id):
+    """
+    Retrieve and display the TODO list progress for a given employee.
+
+    Args:
+        employee_id (int): The ID of the employee for whom to retrieve TODO list progress.
+
+    Returns:
+        None
+    """
+    # Define the URL for the API
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{base_url}/users/{employee_id}"
+    todo_url = f"{base_url}/todos?userId={employee_id}"
+
+    # Fetch user information
+    response_user = requests.get(user_url)
+    user_data = response_user.json()
+    employee_name = user_data.get("name")
+
+    # Fetch TODO list information
+    response_todo = requests.get(todo_url)
+    todo_list = response_todo.json()
+
+    # Calculate progress
+    total_tasks = len(todo_list)
+    done_tasks = sum(1 for task in todo_list if task["completed"])
+
+    # Display progress
+    print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
+    for task in todo_list:
+        if task["completed"]:
+            print(f"\t{task['title']}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+    else:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
