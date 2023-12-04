4743f7d71f537b91a5a95c14677e7bca908d23fa
konditi1 <konditi1@gmail.com>
2023-12-04 05:19:45 +0300

    number of subsribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,29 @@
+import requests
+
+
+def number_of_subscribers(subreddit):
+    # Reddit API endpoint for getting subreddit information
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    # Set a custom User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    # Make a GET request to the Reddit API
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+
+        # Extract the number of subscribers from the response
+        subscribers = data['data']['subscribers']
+
+        return subscribers
+    elif response.status_code == 404:
+        # Subreddit not found, return 0
+        return 0
+    else:
+        # Other error, print the status code and return 0
+        print(f"Error: {response.status_code}")
+        return 0
